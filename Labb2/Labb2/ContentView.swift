e9f0fe08988e5e72ff32a7f3cb03e1c4436ed600
Bashar032 <abosalim2003@gmail.com>
2026-03-08 20:41:56 +0100

    Add files via upload

---
+++ Labb2/Labb2/ContentView.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct Question {
+    let text: String
+    let options: [String]
+    let correctAnswer: String
+}
---
+++ Labb2/Labb2/Labb2App.swift
@@ -0,0 +1,17 @@
+//
+//  Labb2App.swift
+//  Labb2
+//
+//  Created by Bashar Abdulsalam on 2026-03-08.
+//
+
+import SwiftUI
+
+@main
+struct Labb2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
